334c2c365f480dcf54d5a55f80cd1b71836e53cf
BobZhome <BobZhome@Cheerful.com>
2012-03-27 20:29:38 -0400

    update

--- thunderc_MS690.mk
+++ thunderc_MS690.mk
@@ -1,4 +1,5 @@
 # We're on MetroPCS (TODO)
+CDMA_BRAND := metropcs
 CDMA_GOOGLE_BASE := android-metropcs-us
 CDMA_CARRIER_ALPHA := MetroPCS
 CDMA_CARRIER_NUMERIC := 311660
@@ -7,3 +8,9 @@ SUB_MODEL := MS690
 
 # Call the common thunderc stuff first to avoid a lot of duplication.
 $(call inherit-product, device/lge/thunderc_common/thunderc_common.mk)
+
+# Add in model specific kernel modules
+PRODUCT_COPY_FILES += \
+    device/lge/thunderc_MS690/files/kernel/wireless.ko:system/lib/modules/wireless.ko \
+    device/lge/thunderc_MS690/files/kernel/tun.ko:system/lib/modules/tun.ko \
+    device/lge/thunderc_MS690/files/kernel/cifs.ko:system/lib/modules/cifs.ko
